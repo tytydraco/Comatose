30ba20006a99d99539920e788b835b0c67724c8f
tytydraco <tylernij@gmail.com>
2020-01-09 01:43:53 -0800

    Migrate to Kotlin, update to AndroidX, update gradle and libs
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/comatose/MainActivity.java
+++ app/src/main/java/com/draco/comatose/MainActivity.java
@@ -3,16 +3,13 @@ package com.draco.comatose;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.provider.Settings;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
