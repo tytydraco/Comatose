f8ada70e308810ad3ead4d8a09b8c420067060c9
Tyler Nijmeh <tylernij@gmail.com>
2021-04-05 17:58:17 -0700

    Remove useless file
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- app/src/main/java/com/draco/comatose/repositories/profiles/DeviceIdleConstantsProfiles.kt
+++ app/src/main/java/com/draco/comatose/repositories/profiles/DeviceIdleConstantsProfiles.kt
@@ -1,8 +1,7 @@
 package com.draco.comatose.repositories.profiles
 
 object DeviceIdleConstantsProfiles {
-    val DEFAULT =       null
-
+    val DEFAULT =             null
     const val LIGHT =         ""
     const val MODERATE =      ""
     const val HIGH =          ""
